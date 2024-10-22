1cf22b46edd17b4d267f5e4a02f87b1d34e2356d
Dijar06 <dize06001@utb.vaxjo.se>
2024-10-22 15:50:07 +0200

    Add project files.

---
+++ Inlämningsuppgift TryParse/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Security.Cryptography.X509Certificates;
+
+namespace Uppgift
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int resultat;
+            if (!TryParse(Console.ReadLine(), out resultat))
+            {
+                Console.WriteLine("Skriv in en siffra");
+            }
+
+        }
+        public static bool TryParse(string s, out int result)
+        {
+            result = 0;
+
+            try
+            {
+                result = int.Parse(s);
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+    }
+}
